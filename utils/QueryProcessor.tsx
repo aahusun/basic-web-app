6fe806c0277168e00cd84e8304ddb9cb45605de7
Aaron <ahusun@andrew.cmu.edu>
2025-10-07 09:58:18 -0400

    game warmup 2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "ahusun";
+    return "aahusun";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
